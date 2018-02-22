d2cd195aa91d0a9ca7649e3947a9b26b039ce12f
Joseph York <35442212+JosephYork@users.noreply.github.com>
2018-02-23 00:57:51 +1100

    Added discord and spotify installations

---
+++ discord_install.ps1
@@ -0,0 +1,13 @@
+﻿# PowerShell script to download and install Google Chrome
+
+# Set variables
+$discord_path = 'C:\Users\Malore\discord_install.exe'
+
+# Fetch the Google Chrome installer
+(New-Object Net.WebClient).DownloadFile('https://discordapp.com/api/download?platform=win', $discord_path)
+Invoke-WebRequest 'https://discordapp.com/api/download?platform=win' -OutFile $discord_path
+
+# Install Google Chrome if $chrome_path exists
+if([System.IO.File]::Exists($discord_path)){
+    $discord_path
+}
---
+++ spotify_install.ps1
@@ -0,0 +1,13 @@
+﻿# PowerShell script to download and install Google Chrome
+
+# Set variables
+$spotify_path = 'C:\Users\Malore\spotify_install.exe'
+
+# Fetch the Google Chrome installer
+(New-Object Net.WebClient).DownloadFile('https://download.scdn.co/SpotifySetup.exe', $spotify_path)
+Invoke-WebRequest 'https://download.scdn.co/SpotifySetup.exe' -OutFile $spotify_path
+
+# Install Google Chrome if $chrome_path exists
+if([System.IO.File]::Exists($spotify_path)){
+    C:\Users\Malore\spotify_install.exe
+}
